3e6f2762906de4d1c8a8bcd7f5f91fb74683ec40
subzero010 <subzero010@naver.com>
2021-10-18 20:02:46 +0900

    초기환경세팅

---
+++ src/main/java/kr/co/redspring/blog/controller/HelloWorld.java
@@ -0,0 +1,9 @@
+package kr.co.redspring.blog.controller;
+
+import java.util.logging.SocketHandler;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World~");
+    }
+}
